bac760312a2145fb0dc71dd7054ba81fe9bbe900
amr-yassir <amryassir66@gmail.com>
2024-07-01 17:33:31 +0300

    added a test for invalid URls

--- main.go
+++ main.go
@@ -8,12 +8,16 @@ import (
 )
 
 func timeDateHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		time := time.Now().Format(time.RFC1123)
-		fmt.Fprintf(w, "Current date and time: %s", time)
-	} else {
+	if r.Method != "GET" {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
 	}
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	time := time.Now().Format(time.RFC1123)
+	fmt.Fprintf(w, "Current date and time: %s", time)
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -6,23 +6,41 @@ import (
 	"testing"
 	"time"
 )
+
 func TestTimeDateHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-    	t.Fatal(err)
-	}
+	t.Run("Valid URL", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(timeDateHandler)
-	
-	handler.ServeHTTP(rr, req)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(timeDateHandler)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
+		handler.ServeHTTP(rr, req)
 
-	expected := "Current date and time: " + time.Now().Format(time.RFC1123)
-	if rr.Body.String() != expected {
-    	t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		expected := "Current date and time: " + time.Now().Format(time.RFC1123)
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+		}
+	})
+	t.Run("Invalid URL", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/Invalid", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(timeDateHandler)
+
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusNotFound {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+		}
+	})
 }
